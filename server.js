0375f558be0ed072295890ea7c2d0dc0989288ad
Rupal Singh <rupalsingh0711@gmail.com>
2022-04-16 12:30:24 +0530

    c2

--- server.js
+++ server.js
@@ -1,20 +1,26 @@
 //Install express server
+// function requireHTTPS(req, res, next) {
+//     // The 'x-forwarded-proto' check is for Heroku
+//     if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     next();
+// }
+
 const express = require('express');
 const path = require('path');
 
 const app = express();
-
+// app.use(requireHTTPS);
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+//app.use(express.static(__dirname + '/dist'));
+app.use(express.static('./dist/frontend'));
 
 app.get('/*', (req, res)=>{
     console.log(__dirname)
-    res.sendFile(path.join(__dirname+'/src/index.html'))
+    res.sendFile('index.html', {root: 'dist/frontend/'})
 });
-app.get('/login', (req, res)=>{
-    console.log(__dirname)
-    res.sendFile(path.join(__dirname+'/src/app/app-routing.module.ts'));
-});
+
 
 
 // Start the app by listening on the default Heroku port

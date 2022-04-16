e3af0773b9d9f2731f591bb2151f5782ea790626
Rupal Singh <rupalsingh0711@gmail.com>
2022-04-16 12:12:41 +0530

    c1

--- server.js
+++ server.js
@@ -10,9 +10,12 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/*', (req, res)=>{
     console.log(__dirname)
     res.sendFile(path.join(__dirname+'/src/index.html'))
-}
-   
-);
+});
+app.get('/login', (req, res)=>{
+    console.log(__dirname)
+    res.sendFile(path.join(__dirname+'/src/app/app-routing.module.ts'));
+});
+
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
